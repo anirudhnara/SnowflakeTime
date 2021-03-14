e16d005d2efe4dd7e9516af7121b5a4d9c2ef7fe
Mythify <thenoobdude21.12@gmail.com>
2021-03-14 19:37:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import discord
+from discord.utils import snowflake_time
+import datetime
+import time
+
+app = Flask(__name__)
+
+@app.route('/', methods=["POST", "GET"])
+def home():
+	if request.method == "POST":
+		snowflake_id = request.form["id"]
+		created_at = datetime.datetime.strftime(snowflake_time(int(snowflake_id)), "%A, %B %-d, %Y")
+		return render_template("index.html", created_at=created_at, timestamp=round(time.mktime(snowflake_time(int(snowflake_id)).timetuple())))
+	
+	return render_template("index.html")
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=8080, debug=True)
