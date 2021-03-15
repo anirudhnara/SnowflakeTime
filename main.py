f8ccee0ce4f3ad911152e42da80c799aefca635e
Mythify <thenoobdude21.12@gmail.com>
2021-03-15 03:27:55 +0000

    Finishing Touches

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ from flask import Flask, render_template, request, redirect
 import discord
 from discord.utils import snowflake_time
 import datetime
+import requests
 import time
 
 app = Flask(__name__)
 
 @app.route('/', methods=["POST", "GET"])
 def home():
+	
 	if request.method == "POST":
 		try:
 			snowflake_id = request.form["id"]
-			created_at = datetime.datetime.strftime(snowflake_time(int(snowflake_id)), "%A, %B %-d, %Y")
-			return render_template("index.html", created_at=created_at, timestamp=round(time.mktime(snowflake_time(int(snowflake_id)).timetuple())))
+			created_at = f'{datetime.datetime.strftime(snowflake_time(int(snowflake_id)), "%A, %B %-d, %Y, %-I:%M %p")} UTC'
+			timestamp = round(time.mktime(snowflake_time(int(snowflake_id)).timetuple()))
+			iso = snowflake_time(int(snowflake_id))
+			return render_template("index.html", created_at=created_at, timestamp=timestamp, iso=iso)
 		except:
 			return redirect('https://snowflaketime.mythify.repl.co/')
 	
